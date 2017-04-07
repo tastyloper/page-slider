33f329641db974a9159c99309256b71e20f06661
tastyloper <estprocho@gmail.com>
2017-04-07 16:31:15 +0900

    custom pagination add

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,32 @@
-(function($) {
-  "use strict"; // Start of use strict
+$(function() {
 
-  
+  $('#slides').slidesjs({
+    width: 940,
+    height: 2000,
+    pagination: {
+      active: true,
+      effect: "fade"
+    },
+    effect: {
+      fade: {
+        speed: 400
+      }
+    },
+    callback: {
+      complete: function(number) {
+        var datanum = number - 1;
+        // Change slide number on animation complete
+        $('.custom-navigation a').removeClass('active');
+        $('.custom-navigation a[data-slidesjs-item=' + datanum + ']').addClass('active');
+      }
+    }
+  });
 
-})(jQuery); // End of use strict
+  sjs = $('#slides').data('plugin_slidesjs');
+
+  $('.custom-navigation a').on('click', function(clickevent) {
+    var target = Number($(this).attr("data-slidesjs-item")) + 1;
+    sjs.goto(target);
+  });
+
+});
